829a3e7f436f1f540d6458c70c6b8ddb4660a9ea
Jordan Cooper <jordancooper1195@gmail.com>
2024-08-04 20:36:49 +1200

    Changes gitsigns config

--- init.lua
+++ init.lua
@@ -131,10 +131,19 @@ require("lazy").setup({
     "lewis6991/gitsigns.nvim",
     opts = {
       signs = {
-        add = { text = "+" },
-        change = { text = "c" },
-        delete = { text = "d" },
+        add = { text = '+' },
+        change = { text = 'C' },
+        delete = { text = 'D' },
       },
+      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+        delay = 2000,
+        ignore_whitespace = true,
+        virt_text_priority = 100,
+      },
+      current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
     },
   },
 
