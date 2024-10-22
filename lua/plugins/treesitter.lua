dbe48e672792ece5359d39bbe6a8c0afe6e4a80d
Jordan Cooper <jordancooper1195@gmail.com>
2024-10-23 08:24:11 +1300

    Format treesitter ensure_installed-s

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,18 +1,33 @@
 return { "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   opts = {
-    ensure_installed = { "angular", "bash", "css", "diff", "dockerfile", "git_config", "gitignore", "html", "javascript", "json", "lua", "luadoc", "markdown", "markdown_inline", "query", "regex", "scss", "svelte", "typescript", "vim", "vimdoc", "yaml" },
+    ensure_installed = {
+      "angular",
+      "bash",
+      "css",
+      "diff",
+      "dockerfile",
+      "git_config",
+      "gitignore",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "luadoc",
+      "markdown",
+      "markdown_inline",
+      "query",
+      "regex",
+      "scss",
+      "svelte",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "yaml"
+    },
     auto_install = true,
     highlight = { enable = true },
     indent = { enable = true },
-
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<C-Space>",
-        node_incremental = "<C-Space>",
-      },
-    },
   },
 
   config = function(_, opts)
